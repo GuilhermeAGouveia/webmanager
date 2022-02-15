850e14df885d89101faaba8f3647194d11eb3004
GuilhermeAGouveia <guilhermegouveia.dev@gmail.com>
2022-02-15 10:32:49 -0300

    Add function blockMotherPhoneTimer at ./main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import random
 import warnings
+import time
 
 from dotenv import load_dotenv
 from routermanager.enumR import ModeEnum
@@ -10,6 +12,12 @@ load_dotenv()
 
 gw_addr = os.getenv("GATEWAY_ADDRESS")
 
+"""
+    Bloqueio e desbroqueio imediato
+    :param active :type bool indica se devemos bloquear (True) ou desbloquear (False)
+"""
+
+
 def blockMotherPhone(active):
     rm = RouterManager(url=f"http://{gw_addr}")
     rm.login()
@@ -20,9 +28,46 @@ def blockMotherPhone(active):
     rm.close()
 
 
+"""
+    Bloqueia e desbroqueia conexão com roteador, causando sensação de rede instável
+    :param  finalState indica se ao final a conexão é bloqueada (-a), ou desbloquada (-f) [DEFAULT]
+    :param n_interations indica o numero de iterações entre desbloquear e bloquear
+"""
+
+
+def blockMotherPhoneTimer(finalState: str = "-f", n_interations: int = 10) -> None:
+    print("Block timer started!")
+    rm = RouterManager(url=f"http://{gw_addr}")
+    rm.login()
+    advanced = rm.alterMode(ModeEnum.ADVANCED)
+    security = advanced.security()
+    access_control = security.accessControl()
+    toggle = False
+    for i in range(0, n_interations):
+        print(f"Block sequence {i}:")
+        toggle = not toggle
+        access_control.active(toggle)
+        time_wait = random.randrange(3, 6)
+        time.sleep(time_wait)
+
+    if finalState == "-a":
+        access_control.active(True)
+    else:
+        access_control.active(False)
+
+    rm.close()
+
+
 if __name__ == '__main__':
     try:
-        blockMotherPhone(True if sys.argv[1].lower() == '-a' else False)
+        if sys.argv[1].lower() == '-t':
+            try:
+                blockMotherPhoneTimer(n_interations=int(sys.argv[2]), finalState=sys.argv[3].lower())
+            except IndexError:
+                blockMotherPhoneTimer()
+        else:
+            blockMotherPhone(True if sys.argv[1].lower() == '-a' else False)
+
     except IndexError:
         warnings.warn("\033[93m [WARNING] A flag -a or -f não foi indicada, na ausência, -f sera considerado! \033[0m")
         blockMotherPhone(False)
