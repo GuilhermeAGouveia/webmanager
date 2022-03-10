7e82d550caa01057365110e4cf35892926de7667
Guilherme Augusto Gouveia <81968354+GuilhermeAGouveia@users.noreply.github.com>
2022-03-10 14:03:05 -0300

    Add WEBDRIVER_PATH env variable

--- manager.py
+++ manager.py
@@ -1,13 +1,16 @@
+from dotenv import load_dotenv
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options
 
+load_dotenv()
+path = os.getenv("WEBDRIVER_PATH")
 
 class ManagerWeb:
     def __init__(self, browser=Chrome):
         options = Options()
         options.headless = True
         self.driver = browser(options=options,
-                              executable_path="/home/guilherme/Downloads/chromedriver_linux64/chromedriver")
+                              executable_path=path)
 
     def close(self):
         self.driver.close()
